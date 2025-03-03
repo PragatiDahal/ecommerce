b882022b9484fb76d4cda619e42490fa8dc68770
PragatiDahal <pragatidahal2058@gmail.com>
2025-03-03 13:02:20 +0545

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="bg-red-600 text-white">
+       Hello World 
+      </div>
+    </>
+  )
+}
+
+export default App
